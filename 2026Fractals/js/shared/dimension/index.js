84cfe816d64e2c8ed80067a03e064c2e03c740d0
H-Grewcock <hjgrewcock@gmail.com>
2026-06-13 14:29:53 +0100

    Delete 2026Fractals directory

--- 2026Fractals/js/shared/dimension/index.js
+++
@@ -1,4 +0,0 @@
-// js/shared/dimension/index.js:
-export { DimensionLab } from "./DimensionLab.js";
-export { canvasToMask } from "./adapters/canvasMask.js";
-export { boundaryFromClasses, boundaryFromBinaryMask } from "./adapters/boundaryMask.js";
